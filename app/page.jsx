09821ae3f5b44cae8a71b41973ce5835c4a58ca4
Carlos Eduardo Ferraz Augusto <cadu.augusto10@gmail.com>
2025-03-27 10:22:03 -0300

    feat: create code header

--- app/page.jsx
+++ app/page.jsx
@@ -3,8 +3,7 @@ import Header from "../components/Header";
 export default function Home() {
   return (
     <div >
-      <h1>My First NextJS App</h1>
-      <p>Welcome to my first NextJS app!</p>
+      <Header />
     </div>
   );
 }
