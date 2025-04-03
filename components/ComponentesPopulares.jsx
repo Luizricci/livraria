f697f3b9befc55372e2c63cbac19b3d819ad5980
Carlos Eduardo Ferraz Augusto <cadu.augusto10@gmail.com>
2025-04-03 08:56:00 -0300

    feat: create icons categorias populares

--- components/ComponentesPopulares.jsx
+++ components/ComponentesPopulares.jsx
@@ -1,5 +1,10 @@
 import styles from "../styles/ComponentesPopulares.module.css";
 
 export default function ComponentesPopulares({ link, image, text }) {
-
+  return (
+    <a href={link} className={styles.categoriaCard}>
+      <span>{image}</span>
+      <p>{text}</p>
+    </a>
+  );
 }
